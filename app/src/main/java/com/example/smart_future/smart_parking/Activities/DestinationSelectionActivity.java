43db52c5d57492c65adb465feb98e2d5aeea0acb
root <root@Zach-PC.localdomain>
2018-01-04 19:35:59 -0500

    Repo creation

---
+++ app/src/main/java/com/example/smart_future/smart_parking/Activities/DestinationSelectionActivity.java
@@ -0,0 +1,30 @@
+package com.example.smart_future.smart_parking.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.smart_future.smart_parking.R;
+
+public class DestinationSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination_selection);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/smart_future/smart_parking/Activities/RouteSelectionActivity.java
@@ -0,0 +1,15 @@
+package com.example.smart_future.smart_parking.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.smart_future.smart_parking.R;
+
+public class RouteSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_selection);
+    }
+}
